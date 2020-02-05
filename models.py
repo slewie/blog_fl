73cdb65df065b62ea0be3951d1382e827cde9170
slewie <slewie@users.noreply.github.com>
2020-02-05 21:08:08 +0500

    В посты можно добавить тэги, навигация по тэгам, вывод постов с определенным тэгом

--- models.py
+++ models.py
@@ -8,6 +8,12 @@ def slugify(s):
     return re.sub(pattern, '-', s).lower()
 
 
+post_tags = db.Table('post_tags',
+                     db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+                     )
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(140))
@@ -19,9 +25,24 @@ class Post(db.Model):
         super(Post, self).__init__(*args, **kwargs)
         self.generate_slug()
 
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
+
     def generate_slug(self):
         if self.title:
             self.slug = slugify(self.title)
 
     def __repr__(self):
         return f'<Post id: {self.id}, title: {self.title}>'
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100), unique=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return f'<Tag id: {self.id}, name: {self.name}>'
